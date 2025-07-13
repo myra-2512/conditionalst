8734208bc7ab0521cfa69b04ca08ea196baebdcf
myra-2512 <abhi.back2back@gmail.com>
2025-07-13 10:35:48 +0530

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,28 @@
+#Activity1
+num=3
+if num>0:
+    print(num,"is a positive number.")
+
+num=-1
+if num<0:
+    print(num,"is a negatve number.")
+
+#Activity2
+actual_cost=float(input("Enter the cost price of the item:"))
+sale_amount=float(input("Enter the selling price:"))
+
+if (sale_amount > actual_cost):
+    amount=sale_amount-actual_cost
+    print("Total profit is={0}".format(amount))
+
+else:
+    print("Loss incurred in the transaction.")
+#Activity3
+i=int(input("Enter a number:"))
+if(i<15):
+    print("i is smaller than 15")
+    print("This is an if block")
+else:
+    print("i is greater than or equal to 15")
+    print("This is an else block")
+print("I am neither part of if block nor else block")
